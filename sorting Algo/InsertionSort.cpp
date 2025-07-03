f2f28bd2224901eebe9201c47152a3b464385363
Mayank Saini <mayanksaini0416@gmail.com>
2025-07-04 01:00:23 +0530

    insertion sort

---
+++ sorting Algo/InsertionSort.cpp
@@ -0,0 +1,41 @@
+// idea : we will insert 1 elelment in existing sorted array at a time to make the entire data sorted.
+#include <iostream>
+using namespace std;
+
+ void insertionSort(int arr[],int n){
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+} 
+// Function to print the array
+    void print( int arr[], int n) {
+        for(int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+// main function to test the insertion sort 
+int main() {
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Original array: ";
+    print(arr, n);
+    
+    insertionSort(arr, n);
+    
+    cout << "Sorted array: ";
+    print(arr, n);
+    
+    return 0;
+}
