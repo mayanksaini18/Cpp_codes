915415341129e268efb6d2d4599820b45992cfb1
Mayank Saini <mayanksaini0416@gmail.com>
2025-08-08 22:12:53 +0530

    code update

--- rangeQuery/query(s,v).cpp
+++ rangeQuery/query(s,v).cpp
@@ -1,1 +1,16 @@
 //Q(s, e, v)
+void applyRangeUpdates(vector<int>& arr, vector<tuple<int, int, int>>& queries) {
+    int n = arr.size();
+    vector<int> diff(n + 1, 0);
+
+    for (auto [s, e, v] : queries) {
+        diff[s] += v;
+        if (e + 1 < n)
+            diff[e + 1] -= v;
+    }
+
+    arr[0] = diff[0];
+    for (int i = 1; i < n; i++) {
+        arr[i] = arr[i - 1] + diff[i];
+    }
+}
